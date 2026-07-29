1bef37c818888ad6cb6a3192265c48dad99fb499
Chaitanya <chaitanya.chaudhari017@gmail.com>
2026-07-29 23:44:16 +0530

    retriever.py

---
+++ src/retriever.py
@@ -0,0 +1,95 @@
+import os
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+# Path where FAISS index is stored
+
+VECTORSTORE_PATH = "vectorstore/faiss_index"
+
+
+# Embedding model
+EMBEDDING_MODEL = "BAAI/bge-small-en-v1.5"
+
+
+
+def load_vectorstore():
+    """
+    Load existing FAISS vector database
+    """
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=EMBEDDING_MODEL
+    )
+
+
+    vectorstore = FAISS.load_local(
+        VECTORSTORE_PATH,
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+
+    return vectorstore
+
+
+
+
+def retrieve_documents(query, k=3):
+    """
+    Retrieve top-k relevant documents
+    """
+
+
+    vectorstore = load_vectorstore()
+
+
+    results = vectorstore.similarity_search(
+        query,
+        k=k
+    )
+
+
+    return results
+
+
+
+
+
+def print_results(results):
+    """
+    Display retrieved documents
+    """
+
+    for i, doc in enumerate(results):
+
+        print("\n--------------------")
+
+        print(f"Result {i+1}")
+
+        print("--------------------")
+
+        print(doc.page_content)
+
+        print("\nSource:")
+        print(doc.metadata.get("source"))
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    query = input(
+        "Ask your trading question: "
+    )
+
+
+    results = retrieve_documents(
+        query
+    )
+
+
+    print_results(results)
